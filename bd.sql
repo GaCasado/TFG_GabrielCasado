ad05b301050649e5b4a5da682cbe1208a7035257
Gonzalo Casado Valcárcel <gocasado@ucm.es>
2024-02-07 10:49:50 +0100

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -1,2 +1,11 @@
 CREATE TABLE Clasificaciones(
-  
+  año       INT,            
+  deporte   VARCHAR (30),   
+  equipo    VARCHAR (30),
+  genero    VARCHAR (1),    -- 'm' masculino, 'f' femenino
+  puesto    INT,            --Puesto en la clasificacion final, -1 si ha sido descalificado
+  puntos    INT,            
+  asc_desc  INT,            --1 ascenso, -1 descenso, 0 permanencia
+  alfonso   INT,            --Puesto en final, -1 si no se ha clasificado
+  PRIMARY KEY(año, deporte, equipo, genero)
+);
